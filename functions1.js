dfddeffd281b0a5c369464432788f865b9ce3441
sou1exe <113289965+sou1exe@users.noreply.github.com>
2023-12-22 16:00:11 +0100

    done

---
+++ functions1.js
@@ -0,0 +1,60 @@
+//ex1
+var carName = volvo 
+//ex2
+
+var length = 16;//int
+
+var lastName = "Johnson"; //string
+
+var scores = [20, 12, 15, 14, 20, 10, 2]; //int
+
+var person = {
+  firstName: "John",//string
+  lastName: "Doe"//string
+}; 
+
+var isGreaterThan10 = length > 10 //int
+//ex3 
+function square1(x) { 
+    return x * x ; 
+    } 
+   //ex3 
+    function square2(x){
+    return x * x ; 
+    }
+   //ex3 
+    function square3(x){ 
+        return x * x;
+        }
+        //ex4
+        function largest(num1,num2) {
+if (num1>num2)
+return num1+"is bigger"
+else 
+return num2+"is bigger"
+        }
+        //ex5
+        function add(number1, number2) {
+            return number1+number2
+        }
+        //ex6 
+        function subtract(number1, number2) {
+            return number1-number2
+        } 
+            //ex7
+        function addmin(n){
+            var num1 = 2
+            var num2 = 3
+            if (n == '+'){
+            return num1+num2}
+        else 
+        return num1-num2
+    }
+  //ex8
+     function divide(num1,num2) {
+return num1/num2
+     }
+     function multiply(num1,num2) {
+        return num1*num2
+     }
+     
